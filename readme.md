082dc7789d43e227c42a39282ea818c4e97f4f3a
James Dinsdale <hi@molovo.co>
2017-06-15 12:36:05 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Zulu Aliases
+
+A selection of aliases for [`zulu`](https://zulu.sh) commands.
+
+## Installation
+
+```sh
+zulu install zulu-aliases
+```
---
+++ zulu-aliases.plugin.zsh
@@ -0,0 +1,121 @@
+#!/usr/bin/env zsh
+
+# zulu alias
+alias za='zulu alias'
+alias zaa='zulu alias add'
+alias zal='zulu alias load'
+alias zax='zulu alias rm'
+
+# zulu bundle
+alias zb='zulu bundle'
+alias zbc='zulu bundle --cleanup'
+alias zbcf='zulu bundle --cleanup --force'
+alias zbd='zulu bundle --dump'
+alias zbdf='zulu bundle --dump --force'
+
+# zulu cdpath
+alias zc='zulu cdpath'
+alias zca='zulu cdpath add'
+alias zcr='zulu cdpath reset'
+alias zcx='zulu cdpath rm'
+
+# zulu fpath
+alias zfp='zulu fpath'
+alias zfpa='zulu fpath add'
+alias zfpr='zulu fpath reset'
+alias zfpx='zulu fpath rm'
+
+# zulu func
+alias zf='zulu func'
+alias zfa='zulu func add'
+alias zfe='zulu func edit'
+alias zfl='zulu func load'
+alias zfx='zulu func rm'
+
+# zulu info
+alias zin='zulu info'
+
+# zulu link
+alias zl='zulu link'
+
+# zulu list
+alias zls='zulu list'
+alias zla='zulu list --all'
+alias zld='zulu list --describe'
+alias zlda='zulu list --describe --all'
+alias zldi='zulu list --describe --installed'
+alias zldn='zulu list --describe --not-installed'
+alias zldt='zulu list --describe --type'
+alias zlds='zulu list --describe --simple'
+alias zldsa='zulu list --describe --simple --all'
+alias zldsi='zulu list --describe --simple --installed'
+alias zldsn='zulu list --describe --simple --not-installed'
+alias zldst='zulu list --describe --simple --type'
+alias zli='zulu list --installed'
+alias zln='zulu list --not-installed'
+alias zls='zulu list --simple'
+alias zlsa='zulu list --simple --all'
+alias zlsi='zulu list --simple --installed'
+alias zlsn='zulu list --simple --not-installed'
+alias zlst='zulu list --simple --type'
+alias zlt='zulu list --type'
+
+# zulu manpath
+alias zm='zulu manpath'
+alias zma='zulu manpath add'
+alias zmr='zulu manpath reset'
+alias zmx='zulu manpath rm'
+
+# zulu path
+alias zp='zulu path'
+alias zpa='zulu path add'
+alias zpr='zulu path reset'
+alias zpx='zulu path rm'
+
+# zulu theme
+alias zt='zulu theme'
+
+# zulu search
+alias zs='zulu search'
+
+# zulu self-update
+alias zsu='zulu self-update'
+alias zsc='zulu self-update --check'
+
+# zulu switch
+alias zsw='zulu switch'
+alias zsb='zulu switch --branch'
+alias zst='zulu switch --tag'
+
+# zulu sync
+alias zsy='zulu sync'
+alias zsp='zulu sync push'
+alias zsP='zulu sync pull'
+
+# zulu uninstall
+alias zu='zulu uninstall'
+
+# zulu unlink
+alias zul='zulu unlink'
+
+# zulu update
+alias zup='zulu update'
+alias zuc='zulu update --check'
+
+# zulu upgrade
+alias zug='zulu upgrade'
+alias zugc='zulu upgrade --check'
+alias zuy='zulu upgrade -y'
+
+# zulu var
+alias zv='zulu var'
+alias zva='zulu var add'
+alias zvl='zulu var load'
+alias zvx='zulu var rm'
+alias zvpa='zulu var add --private'
+
+# zulu install
+alias zi='zulu install'
+alias zib='zulu install --branch'
+alias zii='zulu install --ignore-dependencies'
+alias zit='zulu install --tag'
